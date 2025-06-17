43918a2d0e8e1545305883ecc213a9612c4a126e
Shekhar <up81.shekhar@gmail.com>
2025-06-17 20:13:21 +0530

    create index route in app.js

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const Listing = require("./models/listing.js");
+const path = require("path");
+
+app.set("view engine","ejs");
+app.set("views",path.join(__dirname,"views"));
+app.use(express.urlencoded({extended : true}));
 
 const Mongo_Link = "mongodb://127.0.0.1:27017/homyGo";
 
@@ -23,3 +29,27 @@ app.listen(8080,()=>{
 app.get("/",(req,res)=>{
     res.send("Root is working bhaiya ji");
 });
+
+//INDEX EJS ROUTE 
+app.get("/listing", async ( req , res ) => {
+    const allistings = await Listing.find({});
+    res.render("listings/index.ejs", {allistings});
+});
+
+// app.get("/testListing",async (req,res)=>{
+//     let sampleListing = new Listing({
+//         title : "My Home",
+//         description : "Mountain view",
+//         price : 1200,
+//         location : "Himachal",
+//         country : "India",
+//     });
+
+//     await sampleListing.save().then(()=>{
+//         console.log("Listing saved");
+//     }).catch((err) => {
+//         console.log(err);
+//     });
+
+//     res.send("Saved successful");
+// });
