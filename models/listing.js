4dbc273b015de77435e4339aa821ab8b6c7bc51b
Shekhar <up81.shekhar@gmail.com>
2025-06-17 10:55:11 +0530

    Update listing.js

--- models/listing.js
+++ models/listing.js
@@ -1,1 +1,31 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
+const listingSchema = new Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    image: {
+        default : "https://cdn.pixabay.com/photo/2024/01/25/12/30/mountain-8531778_1280.jpg",
+        type: String,
+        set: (v) => v === "" ? "https://cdn.pixabay.com/photo/2024/01/25/12/30/mountain-8531778_1280.jpg" : v,
+    },
+    price: {
+        type: Number,
+        required: true,
+    },
+    location: {
+        type: String,
+    },
+    country: {
+        type: String,
+    }
+});
+
+const Listing = mongoose.model("Listing", listingSchema);
+module.exports = Listing;
